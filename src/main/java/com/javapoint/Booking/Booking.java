41fca6e1c2004fb25b64dab20e338f5714aa8fa0
atifahzailani <nuratifahipot@gmail.com>
2022-08-01 07:05:30 +0800

    Create Booking.java

---
+++ src/main/java/com/javapoint/Booking/Booking.java
@@ -0,0 +1,24 @@
+package com.javapoint.Booking;
+
+public class Booking 
+{
+    private String booking;
+
+    public Booking()
+    {
+
+    }
+    public Booking(String booking)
+    {
+        super();
+        this.booking = booking;
+    }
+    public String getBooking()
+    {
+        return booking;
+    }
+    public void setBooking(String booking)
+    {
+        this.booking = booking;
+    }
+}
